408111893191b731a6e6a95dcaa5238a84a1bc65
Rahul Ganesh <rahulganesh694@gmail.com>
2026-09-13 13:16:53 +0530

    first class

---
+++ src/test/java/testPackage/loginPageTest.java
@@ -0,0 +1,14 @@
+package testPackage;
+
+import static org.testng.Assert.assertTrue;
+
+import org.testng.annotations.Test;
+
+public class loginPageTest {
+
+	
+	@Test
+	public void getLink() {
+		assertTrue(true);
+	}
+}
