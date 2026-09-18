374c11000b81d0d2225bacf6be98d042bcdb3709
Rahul Ganesh <rahulganesh694@gmail.com>
2026-09-19 00:07:50 +0530

    Error Rectification

--- src/test/java/testPackage/shoppingPageTest.java
+++ src/test/java/testPackage/shoppingPageTest.java
@@ -7,6 +7,7 @@ import java.time.Duration;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.annotations.Test;
@@ -18,18 +19,22 @@ public class shoppingPageTest {
 
 	@Test
 	public void goCartAndBackVerification() {
-		WebDriver driver = new ChromeDriver();
-		WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(2));
+		ChromeOptions options = new ChromeOptions();
+		options.addArguments("headless");
+		options.addArguments("window-size=1920,1080");
+		WebDriver driver = new ChromeDriver(options);
+		WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(5));
 		driver.get("https://rahulshettyacademy.com/client/#/auth/login");
 		loginPage loginPage = new loginPage(driver);
 		shoppingPage shoppingPage = loginPage.login("rahulganesh6945@gmail.com",
 				"newP@ssword1");
 		wait.until(ExpectedConditions
-				.elementToBeClickable(driver.findElement(By.cssSelector("button[routerlink='/dashboard/cart']"))));
+				.elementToBeClickable(By.cssSelector("button[routerlink='/dashboard/cart']")));
 		driver.findElement(By.cssSelector("button[routerlink='/dashboard/cart']")).click();
 		wait.until(ExpectedConditions
-				.elementToBeClickable(driver.findElement(By.cssSelector("button[routerlink='/dashboard']"))));
+				.elementToBeClickable(By.cssSelector("button[routerlink='/dashboard']")));
 		driver.findElement(By.cssSelector("button[routerlink='/dashboard']")).click();
 		assertEquals(driver.getTitle(), "Let's Shop");
+		driver.quit();
 	}
 }
