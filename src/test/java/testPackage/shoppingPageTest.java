9e8116b4c4aca894711b1ac6c17c75543a132439
Rahul Ganesh <rahulganesh694@gmail.com>
2026-09-20 21:35:41 +0530

    java practice

--- src/test/java/testPackage/shoppingPageTest.java
+++ src/test/java/testPackage/shoppingPageTest.java
@@ -26,13 +26,10 @@ public class shoppingPageTest {
 		WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(5));
 		driver.get("https://rahulshettyacademy.com/client/#/auth/login");
 		loginPage loginPage = new loginPage(driver);
-		shoppingPage shoppingPage = loginPage.login("rahulganesh6945@gmail.com",
-				"newP@ssword1");
-		wait.until(ExpectedConditions
-				.elementToBeClickable(By.cssSelector("button[routerlink='/dashboard/cart']")));
+		shoppingPage shoppingPage = loginPage.login("rahulganesh6945@gmail.com", "newP@ssword1");
+		wait.until(ExpectedConditions.elementToBeClickable(By.cssSelector("button[routerlink='/dashboard/cart']")));
 		driver.findElement(By.cssSelector("button[routerlink='/dashboard/cart']")).click();
-		wait.until(ExpectedConditions
-				.elementToBeClickable(By.cssSelector("button[routerlink='/dashboard']")));
+		wait.until(ExpectedConditions.elementToBeClickable(By.cssSelector("button[routerlink='/dashboard']")));
 		driver.findElement(By.cssSelector("button[routerlink='/dashboard']")).click();
 		assertEquals(driver.getTitle(), "Let's Shop");
 		driver.quit();
